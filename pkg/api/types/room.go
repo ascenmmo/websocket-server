b4a700ccb3f03a63097c809358382df574add65a
temur abdurahmanov <ascemmephone@gmail.com>
2024-11-10 00:21:25 +0300

    try to fix dead lock

--- pkg/api/types/room.go
+++ pkg/api/types/room.go
@@ -3,7 +3,6 @@ package types
 import (
 	"github.com/ascenmmo/websocket-server/internal/connection"
 	"github.com/google/uuid"
-	"sync"
 	"time"
 )
 
@@ -16,7 +15,6 @@ type Room struct {
 	Users []*User
 
 	UpdatedAt time.Time
-	mtx       sync.RWMutex
 }
 
 type User struct {
@@ -26,22 +24,16 @@ type User struct {
 }
 
 func (r *Room) SetUser(user *User) {
-	r.mtx.Lock()
 	r.Users = r.setUser(r.Users, user)
-	r.mtx.Unlock()
 }
 
 func (r *Room) GetUser() (users []*User) {
-	r.mtx.RLock()
 	users = r.Users
-	r.mtx.RUnlock()
 	return
 }
 
 func (r *Room) RemoveUser(user uuid.UUID) {
-	r.mtx.Lock()
 	r.removeFromArray(user)
-	r.mtx.Unlock()
 }
 
 func (r *Room) SetUpdatedAt() {
@@ -63,13 +55,11 @@ func (r *Room) setUser(users []*User, user *User) (allUsers []*User) {
 }
 
 func (r *Room) removeFromArray(userID uuid.UUID) {
-	r.mtx.Lock()
 	for i, user := range r.Users {
 		if user.ID == userID {
 			r.Users = append(r.Users[:i], r.Users[i+1:]...)
 		}
 	}
-	r.mtx.Unlock()
 }
 
 func (r *Room) SetServerID(id uuid.UUID) {
@@ -82,11 +72,9 @@ func (r *Room) SetServerID(id uuid.UUID) {
 }
 
 func (r *Room) RemoveServerID(id uuid.UUID) {
-	r.mtx.Lock()
 	for i, server := range r.ServerID {
 		if server == id {
 			r.ServerID = append(r.ServerID[:i], r.ServerID[i+1:]...)
 		}
 	}
-	r.mtx.Unlock()
 }
