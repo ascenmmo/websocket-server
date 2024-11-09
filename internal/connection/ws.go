6dbc1b201f9892a28e11c4f8b5306f95b7ac0ae7
temur abdurahmanov <ascemmephone@gmail.com>
2024-11-10 01:05:41 +0300

    fix panic

--- internal/connection/ws.go
+++ internal/connection/ws.go
@@ -3,25 +3,29 @@ package connection
 import (
 	"context"
 	"github.com/gorilla/websocket"
+	"sync"
 )
 
 type WebSocketConnection struct {
 	Conn     *websocket.Conn
 	CtxClose context.CancelFunc
+	mutex    sync.Mutex
 }
 
-func (u *WebSocketConnection) GetID() string {
-	return u.Conn.RemoteAddr().String()
+func (ws *WebSocketConnection) GetID() string {
+	return ws.Conn.RemoteAddr().String()
 }
 
-func (u *WebSocketConnection) Write(msg []byte) error {
-	err := u.Conn.WriteMessage(websocket.BinaryMessage, msg)
+func (ws *WebSocketConnection) Write(msg []byte) error {
+	ws.mutex.Lock()
+	defer ws.mutex.Unlock()
+	err := ws.Conn.WriteMessage(websocket.BinaryMessage, msg)
 
 	return err
 }
 
-func (u *WebSocketConnection) Close() {
-	if u.CtxClose != nil {
-		u.CtxClose()
+func (ws *WebSocketConnection) Close() {
+	if ws.CtxClose != nil {
+		ws.CtxClose()
 	}
 }
